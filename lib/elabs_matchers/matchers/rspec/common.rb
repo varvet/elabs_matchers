d5642a3d53506da660c7d08309684b8dd00fe0f6
Nicklas Ramhöj <ramhoj@gmail.com>
2011-06-15 10:32:31 +0200

    Added the only_include matcher.

--- lib/elabs_matchers/matchers/rspec/common.rb
+++ lib/elabs_matchers/matchers/rspec/common.rb
@@ -3,13 +3,14 @@ module ElabsMatchers
     module Rspec
       ##
       #
-      # Asserts if the hash conains the supplied hash.
+      # Asserts if the hash contains the supplied hash.
       # Works with nested hashes and array of hashes.
       #
       # @param [Hash] options      Key => value pairs seperated by comma
       #
       # Example:
       # { "foo" => ['quox', { 'bar' => 'baz'}]}.should contain_hash({ "foo" => [{ "bar" => "baz" }]})
+      # { "foo" => "bar", "baz" => "quox" }.should_not contain_hash({ "baz" => "bar" })
 
       RSpec::Matchers.define :contain_hash do |expected|
         match do |actual|
@@ -33,6 +34,23 @@ module ElabsMatchers
             actual == expected
           end
         end
+      end
+
+      ##
+      #
+      # Asserts if the array contains exactly the supplied elements.
+      # The order of the element do not have to match.
+      #
+      # @param argument list options      comma seperated list of arguments
+      #
+      # Example:
+      # ["foo", "bar"].should only_include("bar", "foo")
+      # ["foo", "bar"].should_not only_include("foo")
+
+      RSpec::Matchers.define :only_include do |*collection|
+        match { |actual| actual.length == collection.length and collection.all? { |element| actual.include?(element) }  }
+        failure_message_for_should { |actual| "Expected #{actual.inspect} to only include #{collection.inspect}." }
+        failure_message_for_should_not { |actual| "Expected #{actual.inspect} to not only include #{collection.inspect}, but it did." }
       end
     end
   end
--- spec/elabs_matchers/matchers/rspec/common_spec.rb
+++ spec/elabs_matchers/matchers/rspec/common_spec.rb
@@ -53,4 +53,22 @@ describe ElabsMatchers::Matchers::Rspec do
       end
     end
   end
+
+  describe "#only_include" do
+    it "returns true when all the elements are passed in the wrong order" do
+      %w[foo bar].should only_include("bar", "foo")
+    end
+
+    it "returns true when all the elements are passed in the correct order" do
+      %w[foo bar].should only_include("foo", "bar")
+    end
+
+    it "returns false when one or more element is missing" do
+      %w[foo bar].should_not only_include("foo")
+    end
+
+    it "returns false when one of the element is not in the list" do
+      %w[foo bar].should_not only_include("foo", "bar", "baz")
+    end
+  end
 end
