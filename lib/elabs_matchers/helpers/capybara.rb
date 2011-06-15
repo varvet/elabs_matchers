a2194c582c66c9dfcdd95139a2f8b11abc3c8743
Nicklas Ramhöj <ramhoj@gmail.com>
2011-06-15 14:16:15 +0200

    Added documentation to the select_year_and_month helper.

--- lib/elabs_matchers/helpers/capybara.rb
+++ lib/elabs_matchers/helpers/capybara.rb
@@ -1,5 +1,16 @@
 module ElabsMatchers
   module Capybara
+    ##
+    #
+    # Selects a year and a month on a rails date select input field.
+    #
+    # @param [String] year      The name of the year used in the select input field
+    # @param [String] month     The name of the month used in the select input field
+    # @param [Hash] options     The label of the select field
+    #
+    # Example:
+    # select_year_and_month("2010", "March", :from => "Birth date")
+
     def select_year_and_month(year, month, options={})
       year_field = find_field(options[:from])
       month_field = find(:id, year_field[:id].sub(/_1i$/, '_2i'))
