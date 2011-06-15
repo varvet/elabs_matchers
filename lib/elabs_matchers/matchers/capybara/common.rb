54c4334d06a7aa499b3977319b41e7b54d33803f
Nicklas Ramhöj <ramhoj@gmail.com>
2011-06-15 15:44:25 +0200

    Added have_table_row matcher.

--- lib/elabs_matchers/matchers/capybara/common.rb
+++ lib/elabs_matchers/matchers/capybara/common.rb
@@ -21,6 +21,43 @@ module ElabsMatchers
         end
         failure_message_for_should_not { |select| "expected options not to include '#{options.flatten.inspect}' but it did." }
       end
+
+      ##
+      #
+      # Asserts if the supplied table row exists in the table
+      #
+      # @param [String] table name            The tables's caption text.
+      # @param [Hash] column value            A hash representing the column name and value in key-value form.
+      #
+      # Example:
+      # table.should have_table_row('Posts', "Title" => "First", :year => "2012")
+
+      RSpec::Matchers.define :have_table_row do |table_name, row|
+        match do |page|
+          table = page.find(:xpath, XPath::HTML.table(table_name))
+          row_index = nil
+
+          row.all? do |header, value|
+            col_index = table.all("thead th").index { |th| th.text.include?(header) }
+            col_index = if col_index then col_index + 1 else 0 end
+
+            current_row_index = table.all("tbody tr").index { |tr| tr.text.include?(value) }
+            correct_row = row_index.nil? || current_row_index == row_index
+            row_index = current_row_index
+
+            correct_row and table.has_xpath?(".//td[#{col_index}][contains(.,'#{value}')]")
+          end
+        end
+
+        failure_message_for_should do |page|
+          table = page.find(:xpath, XPath::HTML.table(table_name))
+          ascii_table = table.all('tr').map do |tr|
+            '| ' + tr.all('td,th').map { |td| td.text.strip.ljust(21) }.join(' | ') + ' |'
+          end.join("\n")
+          "expected #{row.inspect} to be included in the table #{table_name}, but it wasn't:\n\n#{ascii_table}"
+        end
+        failure_message_for_should_not { |page| "expected there to be no table #{table_name} with row #{row.inspect}, but there was." }
+      end
     end
   end
 end
--- spec/elabs_matchers/matchers/capybara/common_spec.rb
+++ spec/elabs_matchers/matchers/capybara/common_spec.rb
@@ -16,4 +16,45 @@ describe ElabsMatchers::Matchers::Capybara do
       html.should_not have_options("A", "C")
     end
   end
+
+  describe "#have_table_row" do
+    let(:html) do
+      Capybara.string(%Q{
+        <table>
+          <caption>Posts</caption>
+          <thead>
+            <tr><th>Title</th><th>Author</th></tr>
+          </thead>
+          <tbody>
+            <tr><td>First</td><td>Adam</td></tr>
+            <tr><td>Second</td><td>David</td></tr>
+          </tbody>
+        </table>
+      })
+    end
+
+    it "returns true when the one of the pairs in the row exists" do
+      html.should have_table_row("Posts", "Title" => "First")
+    end
+
+    it "returns true when the all of the pairs in the row exists" do
+      html.should have_table_row("Posts", "Title" => "First", "Author" => "Adam")
+    end
+
+    it "returns false when the header is wrong and the value is correct" do
+      html.should_not have_table_row("Posts", "Label" => "First")
+    end
+
+    it "returns false when the header is correct and the value is wrong" do
+      html.should_not have_table_row("Posts", "Title" => "Third")
+    end
+
+    it "returns false when on of the pairs is wrong" do
+      html.should_not have_table_row("Posts", "Title" => "First", "Author" => "David")
+    end
+
+    it "returns false when given the value of another column" do
+      html.should_not have_table_row("Posts", "Title" => "Adam")
+    end
+  end
 end
