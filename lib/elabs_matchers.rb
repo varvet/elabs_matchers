56e486ba3cb9f4cd65476cbd8cf272355f0d9d0b
Nicklas Ramhöj <ramhoj@gmail.com>
2011-06-15 13:10:04 +0200

    Requiring all files nessecary.

--- lib/elabs_matchers.rb
+++ lib/elabs_matchers.rb
@@ -1,3 +1,5 @@
 module ElabsMatchers
-  require 'elabs_matchers/matchers/capybara/common.rb'
+  Dir[File.join(File.expand_path(File.dirname(__FILE__)), "elabs_matchers/**/*.rb")].each do |file|
+    require file unless file.split("/").last == "version.rb"
+  end
 end
