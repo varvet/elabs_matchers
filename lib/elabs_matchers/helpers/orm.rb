b437ad9848ef76194d04ecd52c3caa265fbed027
Nicklas Ramhöj <ramhoj@gmail.com>
2011-06-15 13:10:27 +0200

    Added documentation and improved specs for the orm helper methods.

--- lib/elabs_matchers/helpers/orm.rb
+++ lib/elabs_matchers/helpers/orm.rb
@@ -1,8 +1,26 @@
 module ElabsMatchers
   module Orm
+    ##
+    #
+    # Finds the record from the database and return a new instance for that record.
+    #
+    # @param [Object] record      An instance of an ORM record
+    #
+    # Example:
+    # reload(post)
+
     def reload(model)
       model.class.find(model.id)
     end
+
+    ##
+    #
+    # Saves the record and fetches it from the database and return a new instance for that record.
+    #
+    # @param [Object] record      An instance of an ORM record
+    #
+    # Example:
+    # save_and_reload(post)
 
     def save_and_reload(model)
       model.save!
--- spec/elabs_matchers/helpers/orm_spec.rb
+++ spec/elabs_matchers/helpers/orm_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
 describe ElabsMatchers::Orm do
-  describe "#save_and_reload" do
-    let(:post) { Post.create(:title => "New") }
+  let(:post) { Post.create(:title => "New") }
 
+  describe "#save_and_reload" do
     it "it saves the record" do
       post.title = "Updated"
       save_and_reload(post).title.should == "Updated"
@@ -11,6 +11,12 @@ describe ElabsMatchers::Orm do
 
     it "it reloads the record" do
       save_and_reload(post).object_id.should_not == post.object_id
+    end
+  end
+
+  describe "#reload" do
+    it "it reloads the record" do
+      reload(post).object_id.should_not == post.object_id
     end
   end
 end
