683e53de23845de9df404e5edc0a5b4540128ea9
Nicklas Ramhöj <ramhoj@gmail.com>
2011-06-16 15:44:17 +0200

    Adding common helpers to cucumeber if available.

--- lib/elabs_matchers.rb
+++ lib/elabs_matchers.rb
@@ -10,7 +10,10 @@ module ElabsMatchers
     Dir[File.join(File.expand_path(File.dirname(__FILE__)), "elabs_matchers/helpers/*.rb")].each do |file|
       file = file.split("/").last.split(".").first
       file = file[0].chr.upcase + file[1..-1]
-      config.include eval("ElabsMatchers::Helpers::#{file}")
+      module_to_include = eval("ElabsMatchers::Helpers::#{file}")
+
+      config.include(module_to_include)
+      World(module_to_include) if defined?(World)
     end
   end
 end
