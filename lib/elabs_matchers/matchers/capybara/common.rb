8047ff0f64eb5b9c4834a71ec2744ea0229ce2a2
Nicklas Ramhöj <ramhoj@gmail.com>
2011-06-16 11:21:00 +0200

    Added the have_form_errors_on matcher.

--- lib/elabs_matchers/matchers/capybara/common.rb
+++ lib/elabs_matchers/matchers/capybara/common.rb
@@ -151,6 +151,33 @@ module ElabsMatchers
         failure_message_for_should { |page| "expected flash alert to be '#{text}' but was '#{page.find('#flash.alert').text}'" }
         failure_message_for_should_not { |page| "expected flash alert not to be '#{text}' but it was" }
       end
+
+      ##
+      #
+      # Asserts if the supplied flash alert exists or not
+      #
+      # @param [String] label              The label content associated with the field.
+      # @param [String] message             The error message expected.
+      #
+      # Example:
+      # page.should have_form_errors_on("Name", "Can't be blank")
+
+      RSpec::Matchers.define :have_form_errors_on do |field, message|
+        xpath = %Q{..//span[contains(@class,'error')]}
+        match { |page| page.has_field?(field) and page.find_field(field).has_xpath?(xpath, :text => message) }
+
+        failure_message_for_should do |page|
+          error = page.find_field(field).all(:xpath, xpath).first
+          if not error
+            "expected field '#{field}' to have an error, but it didn't"
+          elsif error.text != message
+            "expected error message on #{field} to be '#{message}' but was '#{error.text}'"
+          else
+            raise "This should not happen"
+          end
+        end
+        failure_message_for_should_not { |page| "expected error message on '#{field}' not to be '#{text}' but it was" }
+      end
     end
   end
 end
--- spec/elabs_matchers/matchers/capybara/common_spec.rb
+++ spec/elabs_matchers/matchers/capybara/common_spec.rb
@@ -142,4 +142,28 @@ describe ElabsMatchers::Matchers::Capybara do
       html.should_not have_flash_alert("Success")
     end
   end
+
+  describe "#have_form_errors_on" do
+    let(:html) do
+      Capybara.string(%Q{
+        <form>
+          <span class="error">Can't be blank</span>
+          <label for="name">Name</label>
+          <input type="text" name="name" id="name" value="" />
+        </form>
+      })
+    end
+
+    it "returns true if the label and the error message is correct" do
+      html.should have_form_errors_on("Name", "Can't be blank")
+    end
+
+    it "returns false if the label is correct by the error message is wrong" do
+      html.should_not have_form_errors_on("Name", "Not good enough")
+    end
+
+    it "returns false if the label is wrong by the error message is correct" do
+      html.should_not have_form_errors_on("Author", "Can't be blank")
+    end
+  end
 end
