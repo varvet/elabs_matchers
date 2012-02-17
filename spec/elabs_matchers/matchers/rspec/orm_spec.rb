18be15c4d550253952f552297f08123b255c0e1c
Nicklas Ramhöj <dev+ramhoj@elabs.se>
2012-02-17 14:53:15 +0100

    Move the allow matcher to its own file.

--- spec/elabs_matchers/matchers/rspec/orm_spec.rb
+++ spec/elabs_matchers/matchers/rspec/orm_spec.rb
@@ -13,45 +13,4 @@ describe ElabsMatchers::Matchers::Rspec::Orm do
       post.should_not persist(:title, "Updated")
     end
   end
-
-  describe "#allow" do
-    subject { post }
-
-    context "with one example value" do
-      context "with one attribute" do
-        it { should allow("Elabs").as(:title) }
-        it { should_not allow("").as(:title) }
-
-        it { expect { should allow("").as(:title) }.to fail_assertion }
-        it { expect { should_not allow("Elabs").as(:title) }.to fail_assertion }
-        it { expect { should_not allow("").as(:category) }.to fail_assertion }
-      end
-
-      context "with several attributes" do
-        it { should allow("Elabs").as([:title, :body]) }
-        it { should_not allow("").as([:title, :body]) }
-
-        it { expect { should allow("").as(:title, :body) }.to fail_assertion }
-        it { expect { should_not allow("Elabs").as(:title, :body) }.to fail_assertion }
-      end
-    end
-
-    context "with several example values" do
-      context "with one attribute" do
-        it { should allow("Elabs", "Sweden").as(:title) }
-        it { should_not allow("", nil).as(:title) }
-
-        it { expect { should allow("Elabs", "").as(:title) }.to fail_assertion }
-        it { expect { should_not allow("", "Elabs").as(:title) }.to fail_assertion }
-      end
-
-      context "with several attributes" do
-        it { should allow("Elabs", "Sweden").as(:title, :body) }
-        it { should_not allow("", nil).as(:title, :body) }
-
-        it { expect { should allow("Elabs", "").as(:title, :body) }.to fail_assertion }
-        it { expect { should_not allow("", "Elabs").as(:title, :body) }.to fail_assertion }
-      end
-    end
-  end
 end
