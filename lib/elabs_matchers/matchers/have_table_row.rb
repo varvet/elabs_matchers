21741c5172adba376ef5d2b5a5975ce4806ac468
Anders Carlsson and Jonas Nicklas <dev+andersc+jonas@elabs.se>
2015-06-18 09:42:50 +0200

    Revert addition of strict mode for have_table_row matcher
    Revert "Only look up table row once in has_table_row matcher."
    
    This reverts commit ef0a67dbaa5d46b8856a8ebcd6e8d4283fb60cdf.
    
    Revert "DRY up call to table.has_selector?"
    
    This reverts commit fe1b5a60de07e0c5ea799cf4806fc11340136b05.
    
    Revert "Memoize table_row_matchers."
    
    This reverts commit 08d869aee1e170f8bf9fd87b9ed29a5e58cd1228.
    
    Revert "Optimize form element fallback lookup."
    
    This reverts commit f5849962d6082bf39f577b23119e09487f0bf51c.
    
    Revert "Allow strict matching on table rows."
    
    This reverts commit 4cd6d0e6a4d49d7ff4a6ab7c9830a1c36235aca6.

--- lib/elabs_matchers/matchers/have_table_row.rb
+++ lib/elabs_matchers/matchers/have_table_row.rb
@@ -4,43 +4,26 @@ module ElabsMatchers
       rspec :type => :request
       rspec :type => :feature
 
-      class HaveTableRowMatcher
-        attr_reader :page, :table_name, :row, :options
-
-        def initialize(table_name, row, options = {})
-          @table_name = table_name
-          @row = row
-          @options = options
-        end
+      class HaveTableRowMatcher < Struct.new(:table_name, :row)
+        attr_reader :page
 
         def matches?(page)
           @page = page
-
-          if has_selector? and options[:strict]
-            matches_exactly?
-          else
-            has_selector?
-          end
+          table and table.has_selector?(selector_type, selector)
         end
 
         def does_not_match?(page)
           @page = page
-          row_not_found = (!table or table.has_no_selector?(selector_type, selector))
-
-          if not row_not_found and options[:strict]
-            !matches_exactly?
-          else
-            row_not_found
-          end
+          !table or table.has_no_selector?(selector_type, selector)
         end
 
         def failure_message
-          "Expected #{row.inspect} to be included in the table #{table_name}, but it wasn't:\n\n#{ascii_table}#{strict_match_row_message}"
+          "Expected #{row.inspect} to be included in the table #{table_name}, but it wasn't:\n\n#{ascii_table}"
         end
         alias_method :failure_message_for_should, :failure_message
 
         def failure_message_when_negated
-          "Expected there to be no table #{table_name} with row #{row.inspect}, but there was.\n\n#{ascii_table}#{strict_match_row_message}"
+          "Expected there to be no table #{table_name} with row #{row.inspect}, but there was.\n\n#{ascii_table}"
         end
         alias_method :failure_message_for_should_not, :failure_message_when_negated
 
@@ -51,118 +34,72 @@ module ElabsMatchers
         end
 
         def selector
-          @selector ||= begin
-            if ElabsMatchers.table_row_selector
-              ElabsMatchers.table_row_selector[row, table]
-            else
-              exps = row.map do |header, value|
-                col_index = table.all("th").to_a.index { |th| th.text.include?(header.to_s) }
-                col_index = if col_index then col_index + 1 else 0 end
+          if ElabsMatchers.table_row_selector
+            ElabsMatchers.table_row_selector[row, table]
+          else
+            exps = row.map do |header, value|
+              col_index = table.all("th").to_a.index { |th| th.text.include?(header.to_s) }
+              col_index = if col_index then col_index + 1 else 0 end
 
-                XPath.generate do |x|
-                  cell = x.child(:td, :th)[col_index.to_s.to_sym]
+              XPath.generate do |x|
+                cell = x.child(:td, :th)[col_index.to_s.to_sym]
 
-                  if value.blank?
-                    cell["not(node())".to_sym].or(cell.descendant(:input)["string-length(normalize-space(@value))=0".to_sym])
-                  else
-                    cell[x.contains(value).or(x.descendant(:input)[x.attr(:value).contains(value)])]
-                  end
+                if value.blank?
+                  cell["not(node())".to_sym].or(cell.descendant(:input)["string-length(normalize-space(@value))=0".to_sym])
+                else
+                  cell[x.contains(value).or(x.descendant(:input)[x.attr(:value).contains(value)])]
                 end
               end
-
-              XPath.descendant(:tr)[exps.reduce(:&)]
             end
+
+            XPath.descendant(:tr)[exps.reduce(:&)]
           end
         end
 
         def table
-          @table ||= begin
-            if table_name.respond_to?(:tag_name) and table_name.tag_name == "table"
-              table_name
-            else
-              table_xpath = XPath::HTML.table(table_name)
+          if table_name.respond_to?(:tag_name) and table_name.tag_name == "table"
+            table_name
+          else
+            table_xpath = XPath::HTML.table(table_name)
 
-              if page.has_xpath?(table_xpath)
-                page.find(:xpath, table_xpath)
-              end
+            if page.has_xpath?(table_xpath)
+              page.find(:xpath, table_xpath)
             end
           end
         end
 
         def ascii_table
-          @ascii_table ||= begin
-            synchronize do
-              if table
-                column_lengths = []
-                table.all("tr").map do |tr|
-                  tr.all("td,th").each_with_index do |td, i|
-                    size = td_content(td).strip.size
-                    if (column_lengths[i] || 0) < size
-                      column_lengths[i] = size
-                    end
+          synchronize do
+            if table
+              column_lengths = []
+              table.all("tr").map do |tr|
+                tr.all("td,th").each_with_index do |td, i|
+                  size = td_content(td).strip.size
+                  if (column_lengths[i] || 0) < size
+                    column_lengths[i] = size
                   end
                 end
-
-                table.all("tr").map do |tr|
-                  middle = []
-                  space = if tr.all("td,th").first.tag_name == "th" then "_" else " " end
-                  wall = "|"
-
-                  tr.all("td,th").each_with_index do |td, i|
-                    middle << td_content(td).strip.ljust(column_lengths[i], space)
-                  end
-
-                  [wall, space, middle.join(space + wall + space), space, wall].join
-                end.join("\n")
               end
+
+              table.all("tr").map do |tr|
+                middle = []
+                space = if tr.all("td,th").first.tag_name == "th" then "_" else " " end
+                wall = "|"
+
+                tr.all("td,th").each_with_index do |td, i|
+                  middle << td_content(td).strip.ljust(column_lengths[i], space)
+                end
+
+                [wall, space, middle.join(space + wall + space), space, wall].join
+              end.join("\n")
             end
           end
         end
 
         def td_content(td)
           text = td.text.presence
-          text ||= if form_element = td.first("input, textarea")
-            form_element[:value].presence
-          end
+          text ||= td.find("input, textarea")[:value] if td.has_css?("input")
           text || ""
-        end
-
-        def table_headers
-          @table_headers ||= table.all("th").map(&:text)
-        end
-
-        def row_values
-          @row_values ||= begin
-            real.all("td").map do |td|
-              td_content(td)
-            end
-          end
-        end
-
-        def to_hash
-          @to_hash ||= begin
-            table_headers.zip(row_values).to_h if has_selector?
-          end
-        end
-
-        def matches_exactly?
-          to_hash == row
-        end
-
-        def has_selector?
-          !!real
-        end
-
-        def real
-          @real ||= begin
-            table.first(selector_type, selector) if table
-          end
-        end
-
-        def strict_match_row_message
-          if options[:strict]
-            "\n\nStrict match was made against row: #{to_hash.inspect}"
-          end
         end
 
         def synchronize
@@ -180,15 +117,13 @@ module ElabsMatchers
       # Asserts if the supplied table row exists in the table
       #
       # @param [String] table_name            The tables's caption text.
-      # @param [Hash]   row                   A hash representing the column name and value in key-value form.
-      # @param [Hash]   options               strict: true, require row to match all columns in matching row. Does not work if the table contains duplicate rows.
+      # @param [Hash] row                     A hash representing the column name and value in key-value form.
       #
       # Example:
       # table.should have_table_row("Posts", "Title" => "First", :year => "2012")
-      # table.should have_table_row("Posts", { "Title" => "First", :year => "2012" }, strict: true)
 
-      def have_table_row(table_name, row, options = {})
-        HaveTableRowMatcher.new(table_name, row, options)
+      def have_table_row(table_name, row)
+        HaveTableRowMatcher.new(table_name, row)
       end
     end
   end
